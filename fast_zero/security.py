6ea247fa905dcd69ed4d8885c307e9e49a51b344
otavmacedo <otavmacedo04@gmail.com>
2024-12-04 17:34:11 -0300

    Adicionando get_current_user

--- fast_zero/security.py
+++ fast_zero/security.py
@@ -1,13 +1,24 @@
 from datetime import datetime, timedelta
+from http import HTTPStatus
 
-from jwt import encode
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jwt import decode, encode
+from jwt.exceptions import PyJWTError
 from pwdlib import PasswordHash
+from sqlalchemy import Select
+from sqlalchemy.orm import Session
 from zoneinfo import ZoneInfo
+
+from fast_zero.database import get_session
+from fast_zero.models import User
+
+pwd_context = PasswordHash.recommended()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
 SECRET_KEY = 'your-secret-ket'
 ALGORITHM = 'HS256'
 ACESS_TOKEN_EXPIRE_MINUTES = 30
-pwd_context = PasswordHash.recommended()
 
 
 def get_password_hash(password: str):
@@ -26,3 +37,27 @@ def create_acess_token(data: dict):
     to_encode.update({'exp': expire})
     encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def get_current_user(
+    session: Session = Depends(get_session),
+    token: str = Depends(oauth2_scheme),
+):
+    credentials_exception = HTTPException(
+        status_code=HTTPStatus.UNAUTHORIZED,
+        detail='Could not validate credentials',
+        headers={'WWW-Authenticate': 'Bearer'},
+    )
+    try:
+        payload = decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get('sub')
+        if not username:
+            raise credentials_exception
+    except PyJWTError:
+        raise credentials_exception
+
+    user = session.scalar(Select(User).where(User.email == username))
+    if not user:
+        raise credentials_exception
+
+    return user
