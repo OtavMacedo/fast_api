aa12264ea40d0ff360387e0784e71b3240326563
otavmacedo <otavmacedo04@gmail.com>
2024-12-04 17:35:30 -0300

    Adicionando teste jwt invalido

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,13 +1,24 @@
+from http import HTTPStatus
+
 from jwt import decode
 
 from fast_zero.security import ALGORITHM, SECRET_KEY, create_acess_token
 
 
 def test_jwt():
-    data = {'test': 'test'}
+    data = {'sub': 'test'}
     token = create_acess_token(data)
 
     decoded = decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
-    assert decoded['test'] == 'test'
+    assert decoded['sub'] == 'test'
     assert decoded['exp']
+
+
+def test_jwt_invalid_token(client):
+    response = client.delete(
+        '/users/1', headers={'Authorization': 'Bearer token-invalido'}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
