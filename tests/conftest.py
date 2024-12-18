8a5fa86912d15f90560e2d757355648a90e5ca2f
otavmacedo <otavmacedo04@gmail.com>
2024-12-18 20:14:38 -0300

    Criando UserFactory e otheruser

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 from contextlib import contextmanager
 from datetime import datetime
 
+import factory
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, event
@@ -11,6 +12,15 @@ from fast_zero.app import app
 from fast_zero.database import get_session
 from fast_zero.models import User, table_registry
 from fast_zero.security import get_password_hash
+
+
+class UserFactory(factory.Factory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: f'test{n}')
+    email = factory.LazyAttribute(lambda obj: f'{obj.username}@test.com')
+    password = factory.LazyAttribute(lambda obj: f'{obj.username}123')
 
 
 @pytest.fixture
@@ -64,11 +74,8 @@ def mock_db_time():
 @pytest.fixture
 def user(session):
     password = 'teste123'
-    user = User(
-        username='UserTeste',
-        password=get_password_hash(password),
-        email='teste@teste.com',
-    )
+    user = UserFactory(password=get_password_hash(password))
+
     session.add(user)
     session.commit()
     session.refresh(user)
@@ -79,9 +86,19 @@ def user(session):
 
 
 @pytest.fixture
+def other_user(session):
+    user = UserFactory()
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
+
+
+@pytest.fixture
 def token(client, user):
     response = client.post(
-        '/token',
+        '/auth/token',
         data={'username': user.email, 'password': user.clean_password},
     )
     return response.json()['access_token']
